897403960ba21d76f97b7c730516cb65867346d8
mikgir <groshevm@mail.ru>
2022-10-04 13:21:42 +0300

    lesson_6 task_1

--- highload/public/index.php
+++ highload/public/index.php
@@ -3,34 +3,34 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-//define('LARAVEL_START', microtime(true));
-//
-///*
-//|--------------------------------------------------------------------------
-//| Check If The Application Is Under Maintenance
-//|--------------------------------------------------------------------------
-//|
-//| If the application is in maintenance / demo mode via the "down" command
-//| we will load this file so that any pre-rendered content can be shown
-//| instead of starting the framework, which could cause an exception.
-//|
-//*/
-//
-//if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-//    require $maintenance;
-//}
-//
-///*
-//|--------------------------------------------------------------------------
-//| Register The Auto Loader
-//|--------------------------------------------------------------------------
-//|
-//| Composer provides a convenient, automatically generated class loader for
-//| this application. We just need to utilize it! We'll simply require it
-//| into the script here so we don't need to manually load our classes.
-//|
-//*/
+define('LARAVEL_START', microtime(true));
 
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+//
 require __DIR__.'/../vendor/autoload.php';
 
 /*
@@ -43,27 +43,27 @@ require __DIR__.'/../vendor/autoload.php';
 | to this client's browser, allowing them to enjoy our application.
 |
 */
-//
-//$app = require_once __DIR__.'/../bootstrap/app.php';
-//
-//$kernel = $app->make(Kernel::class);
-//
-//$response = $kernel->handle(
-//    $request = Request::capture()
-//)->send();
-//
-//$kernel->terminate($request, $response);
 
-require_once 'MemcachedService.php';
-require_once 'RedisService.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-$memcached = new MemcachedService();
-$memcached->getCache();
+$kernel = $app->make(Kernel::class);
 
-echo '===========================================';
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-$redis = new RedisService();
-$redis->getCache();
+$kernel->terminate($request, $response);
+//
+//require_once 'MemcachedService.php';
+//require_once 'RedisService.php';
+//
+//$memcached = new MemcachedService();
+//$memcached->getCache();
+//
+//echo '===========================================';
+//
+//$redis = new RedisService();
+//$redis->getCache();
 
 
 
